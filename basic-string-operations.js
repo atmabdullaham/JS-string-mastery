3b66f86dd47fa9a9faf72dbb5532b7f8884ddf5a
atmabdullaham <atmabdullaham@gmail.com>
2024-11-18 23:29:07 +0600

    first commit

--- basic-string-operations.js
+++ basic-string-operations.js
@@ -8,3 +8,9 @@ console.log(str.length); // 13
 const status = "Hero, Programmer"
 console.log(status);
 console.log(status.length);
+
+// 2. Accessing Characters
+// Access the first and last characters of a string.
+
+console.log(str[0]); // 'H'
+console.log(str[str.length - 1]); // '!'
