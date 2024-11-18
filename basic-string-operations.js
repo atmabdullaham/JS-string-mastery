efcf8bc1366e5a70836706a1a8741050c83c2d5e
atmabdullaham <atmabdullaham@gmail.com>
2024-11-18 23:36:00 +0600

    added three basic string operations 1, string creation and length, 2, Accessing Characters, and 3, Changing Case.

--- basic-string-operations.js
+++ basic-string-operations.js
@@ -1,4 +1,4 @@
-//1. String Creation & Length
+//____________________________1. String Creation & Length_______________________________
 
 //Create a string and find its length.
 
@@ -9,8 +9,19 @@ const status = "Hero, Programmer"
 console.log(status);
 console.log(status.length);
 
-// 2. Accessing Characters
+//_____________________________2. Accessing Characters__________________________________
 // Access the first and last characters of a string.
 
 console.log(str[0]); // 'H'
 console.log(str[str.length - 1]); // '!'
+
+
+console.log(status[0]); // 'H'
+console.log(status[status.length - 1]); // 'r'
+
+
+//______________________________3. Changing Case_________________________________________
+// Convert strings to uppercase and lowercase
+
+console.log(status.toUpperCase()); //HERO, PROGRAMMER
+console.log(status.toLowerCase()); //hero, programmer
