aa44b37f05a277a392dbc7e210ecd242e2a44057
atmabdullaham <atmabdullaham@gmail.com>
2024-11-18 23:25:22 +0600

    first commit

---
+++ basic-string-operations.js
@@ -0,0 +1,10 @@
+//1. String Creation & Length
+
+//Create a string and find its length.
+
+const str = "Hello, World!";
+console.log(str.length); // 13
+
+const status = "Hero, Programmer"
+console.log(status);
+console.log(status.length);
